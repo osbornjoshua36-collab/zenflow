b73f6241450cdcf7f65fb405d0ed34d402789d34
Joshua Osborn <osborn.joshua36@gmail.com>
2026-06-02 19:01:16 +0000

    File changes

---
+++ src/pages/Scheduling.jsx
@@ -0,0 +1,9 @@
+import AppointmentCalendar from '@/components/AppointmentCalendar';
+
+export default function Scheduling() {
+  return (
+    <div className="space-y-6">
+      <AppointmentCalendar />
+    </div>
+  );
+}
